2220995ce5abcabe1b95820ad0f4ce1138bf8c7c
Oleksandr Havryliuk <87950995+Sercezhar@users.noreply.github.com>
2023-01-18 01:15:09 +0200

    update Pagination.jsx

--- src/components/Pagination/Pagination.jsx
+++ src/components/Pagination/Pagination.jsx
@@ -19,42 +19,46 @@ function Pagination({ currentPage, total, limit, onPageChange }) {
   const isLastPage = currentPage === pagesCount;
 
   return (
-    <div className={styles.wrapper}>
-      <ul className={styles.pagination}>
-        <PaginationItem
-          page={isTabletOrDesktop ? 'First' : <RxDoubleArrowLeft />}
-          currentPage={currentPage}
-          onPageChange={() => onPageChange(1)}
-          isDisabled={isFirstPage}
-        />
-        <PaginationItem
-          page={isTabletOrDesktop ? 'Prev' : <RxChevronLeft />}
-          currentPage={currentPage}
-          onPageChange={() => onPageChange(currentPage - 1)}
-          isDisabled={isFirstPage}
-        />
-        {pages.map(page => (
-          <PaginationItem
-            key={page}
-            page={page}
-            currentPage={currentPage}
-            onPageChange={onPageChange}
-          />
-        ))}
-        <PaginationItem
-          page={isTabletOrDesktop ? 'Next' : <RxChevronRight />}
-          currentPage={currentPage}
-          onPageChange={() => onPageChange(currentPage + 1)}
-          isDisabled={isLastPage}
-        />
-        <PaginationItem
-          page={isTabletOrDesktop ? 'Last' : <RxDoubleArrowRight />}
-          currentPage={currentPage}
-          onPageChange={() => onPageChange(pagesCount)}
-          isDisabled={isLastPage}
-        />
-      </ul>
-    </div>
+    <>
+      {total > 0 ? (
+        <div className={styles.wrapper}>
+          <ul className={styles.pagination}>
+            <PaginationItem
+              page={isTabletOrDesktop ? 'First' : <RxDoubleArrowLeft />}
+              currentPage={currentPage}
+              onPageChange={() => onPageChange(1)}
+              isDisabled={isFirstPage}
+            />
+            <PaginationItem
+              page={isTabletOrDesktop ? 'Prev' : <RxChevronLeft />}
+              currentPage={currentPage}
+              onPageChange={() => onPageChange(currentPage - 1)}
+              isDisabled={isFirstPage}
+            />
+            {pages.map(page => (
+              <PaginationItem
+                key={page}
+                page={page}
+                currentPage={currentPage}
+                onPageChange={onPageChange}
+              />
+            ))}
+            <PaginationItem
+              page={isTabletOrDesktop ? 'Next' : <RxChevronRight />}
+              currentPage={currentPage}
+              onPageChange={() => onPageChange(currentPage + 1)}
+              isDisabled={isLastPage}
+            />
+            <PaginationItem
+              page={isTabletOrDesktop ? 'Last' : <RxDoubleArrowRight />}
+              currentPage={currentPage}
+              onPageChange={() => onPageChange(pagesCount)}
+              isDisabled={isLastPage}
+            />
+          </ul>
+        </div>
+      ) : null}
+    </>
   );
 }
 
